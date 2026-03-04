f9c3a23f0efb068cd68ddb8b954ee950ba8c9a3a
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-03-04 14:53:19 +0530

    Change nullable to true in teacher profile

--- db_models.py
+++ db_models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from sqlalchemy import String, func, Text, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -20,8 +21,8 @@ class TeacherProfile(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     teacher_id: Mapped[int] = mapped_column(ForeignKey("teacher.id", ondelete="CASCADE"))
-    qualifications : Mapped[str] = mapped_column(String(300), nullable=False)
-    department : Mapped[str] = mapped_column(String(250), nullable=False)
-    office_no : Mapped[str] = mapped_column(String(30), nullable=False)
-    bio : Mapped[Text] = mapped_column(nullable=False)
+    qualifications : Mapped[Optional[str]] = mapped_column(String(300), nullable=True)
+    department : Mapped[Optional[str]] = mapped_column(String(250), nullable=True)
+    office_number : Mapped[Optional[str]] = mapped_column(String(30), nullable=True)
+    bio : Mapped[Text] = mapped_column(nullable=True)
 
